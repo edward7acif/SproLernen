88ee41d53ba76c93bd1e6a47600796477c37b704
Sameh Edward <edw21629@spengergasse.at>
2021-12-22 11:29:38 +0100

    Reqmaping

--- src/main/java/at/spengergasse/sprolernen/domain/ErsteContolle.java
+++ src/main/java/at/spengergasse/sprolernen/domain/ErsteContolle.java
@@ -1,10 +1,12 @@
 package at.spengergasse.sprolernen.domain;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ErsteContolle {
 
+    @RequestMapping(value = "/")
     public String halloControlle()
     {
         return "Hallooo Edward";
