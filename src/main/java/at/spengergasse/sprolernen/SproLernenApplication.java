62d43a42c3a6781d649183c709af8c251ef4f90a
Sameh Edward <edw21629@spengergasse.at>
2021-12-22 11:01:07 +0100

    erstre commit

---
+++ src/main/java/at/spengergasse/sprolernen/SproLernenApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.sprolernen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproLernenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SproLernenApplication.class, args);
+    }
+
+}
---
+++ src/main/java/at/spengergasse/sprolernen/domain/ErsteContolle.java
@@ -0,0 +1,12 @@
+package at.spengergasse.sprolernen.domain;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErsteContolle {
+
+    public String halloControlle()
+    {
+        return "Hallooo Edward";
+    }
+}
